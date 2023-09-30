10ad4cd2b4cd0b9a36d4cf254b7ad7a50d000b97
Aswin Benny <110408942+aswinbennyofficial@users.noreply.github.com>
2023-09-30 08:57:23 +0530

    Update server.js
    added product route

--- server.js
+++ server.js
@@ -1,26 +1,53 @@
+// Load environment variables from .env file
+require('dotenv').config() 
 
-require('dotenv').config()
+// Import the Express.js framework
 const express = require('express')
 const app = express()
 
+// Import Mongoose, an ODM for MongoDB
+const mongoose = require('mongoose'); 
 
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
 
-const mongoose = require('mongoose');
 
+// Defining a route for the root URL ('/')
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+// Define a POST route for '/product'
+app.post('/product', (req, res) => {
+    try {
+        
+        console.log(req.body);
+        res.send(req.body);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Uses a dynamic port if 3000 replaced with 0
+const server = app.listen(0, () => {
+    
+    // saves the assigned port to the port variable
+    const port = server.address().port;
+
+    console.log(`App running on port ${port}`);
+});
+
+//MONGO_URI is the environment variable defined in .env file
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Connected!')).catch((error)=>{
-    console.log(error);
+  .then(() => {
+    
+    console.log('Connected to mongodb!');
+    
   });
 
 
 
 
 
-app.get('/', (req, res) => {
-  res.send('hello worlds')
-})
-
-app.listen(3000,()=>{
-    console.log('app running in port 3000');
-})
