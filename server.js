c089fa442731e6f081dc0743cb2611c32612a240
aswinbennyofficial <aswinbenny.official@gmail.com>
2023-09-30 15:14:32 +0530

    added get from mongodb option

--- server.js
+++ server.js
@@ -21,13 +21,28 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
+
+app.get('/product', async(re,res) => {
+    try{
+
+        const products= await Product.find({});
+        res.status(200).json(products);
+
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
 // Define a POST route for '/product'
 app.post('/product', async(req, res) => {
     try {
         
         // Create a new product document in MongoDB using the data from req.body
         const product=await Product.create(req.body);
-        
+
         res.status(200).json(product);
 
     } catch (error) {
